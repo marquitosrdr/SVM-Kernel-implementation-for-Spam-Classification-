3830656b50e8e90a9fe7e53affeacb07d9f0daa2
Marcos Rusinol de Rueda <42815633+marquitosrdr@users.noreply.github.com>
2019-01-27 19:04:30 +0100

    main.m
    Main part of the algorithm

--- main.m
+++ main.m
@@ -8,5 +8,5 @@ x             = emailFeatures(word_indices);
 p = svmPredict(model, x);
 
 fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
-fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
+fprintf('(1 indicates spam, 0 indicates not spam)\n\n')
 
