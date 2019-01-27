c6734a3b642df00dbc0baf8b6955e8e357cd5f0a
Marcos Rusinol de Rueda <42815633+marquitosrdr@users.noreply.github.com>
2019-01-27 19:10:20 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,12 +1,13 @@
-% To upload the mail to classify
+% Email to classify (change it to emailSample1.txt to get a no-spam classification)
 filename = 'spamSample1.txt';
 
 % Read and predict
+% Extract Features
 file_contents = readFile(filename);
 word_indices  = processEmail(file_contents);
-x             = emailFeatures(word_indices);
-p = svmPredict(model, x);
+features = emailFeatures(word_indices);
+p = svmPredict(model, features);
 
 fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
-fprintf('(1 indicates spam, 0 indicates not spam)\n\n')
+fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
 
